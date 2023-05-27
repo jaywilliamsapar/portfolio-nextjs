0e30f46b328591f70fca132f17a68110a130e7a4
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2023-05-27 23:37:19 +0800

    Contact Updated

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -10,7 +10,7 @@ import ContactImg from '../public/assets/contact.jpg';
 const Contact = () => {
   return (
     <div id='contact' className='w-full lg:h-screen'>
-      <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
           Contact
         </p>
@@ -31,18 +31,18 @@ const Contact = () => {
                 <p>Front-End Developer</p>
                 <p className='py-4'>
                   I am available for freelance or full-time positions. Contact
-                  me and let&apos;s talk.
+                  me and let&apos;s talk. 	
                 </p>
               </div>
               <div>
-                <p className='uppercase pt-8 font-semibold'>Connect With Me</p>
-                <div className='flex items-center justify-between py-4'>
+                <p className='uppercase pt-8 font-bold text-center'>Connect With Me</p>
+                <div className='flex items-center justify-between py-12'>
                   <a
                     href='https://www.linkedin.com/in/clint-briley-50056920a/'
                     target='_blank'
                     rel='noreferrer'
                   >
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-125 ease-in duration-300 bg-[#f8e5dd]'>
                       <FaLinkedinIn />
                     </div>
                   </a>
@@ -51,17 +51,17 @@ const Contact = () => {
                     target='_blank'
                     rel='noreferrer'
                   >
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                      <FaGithub />
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-125 ease-in duration-300 bg-[#f8e5dd]'>
+                      <FaGithub width='30'/>
                     </div>
                   </a>
 
-                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-125 ease-in duration-300 bg-[#f8e5dd]'>
                     <AiOutlineMail />
                   </div>
                   <Link href='/resume'>
                     <a>
-                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-125 ease-in duration-300 bg-[#f8e5dd]'>
                         <BsFillPersonLinesFill />
                       </div>
                     </a>
@@ -72,16 +72,16 @@ const Contact = () => {
           </div>
 
           {/* right */}
-          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4 bg-[#f8e5dd]'>
             <div className='p-4'>
               <form
                 action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
                 method='POST'
                 encType='multipart/form-data'
               >
-                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                  <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>Name</label>
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2 text-[#006964] font-semibold'>
+                  <div className='flex flex-col '>
+                    <label className=' text-sm py-2'>Name</label>
                     <input
                       className='border-2 rounded-lg p-3 flex border-gray-300'
                       type='text'
@@ -89,7 +89,7 @@ const Contact = () => {
                     />
                   </div>
                   <div className='flex flex-col'>
-                    <label className='uppercase text-sm py-2'>
+                    <label className=' text-sm py-2'>
                       Phone Number
                     </label>
                     <input
@@ -100,30 +100,30 @@ const Contact = () => {
                   </div>
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label className='uppercase text-sm py-2'>Email</label>
+                  <label className=' text-sm py-2 text-[#006964] font-semibold'>Email</label>
                   <input
-                    className='border-2 rounded-lg p-3 flex border-gray-300'
+                    className='border-2 rounded-lg p-3 flex border-gray-300 text-[#006964]'
                     type='email'
                     name='email'
                   />
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label className='uppercase text-sm py-2'>Subject</label>
+                  <label className=' text-sm py-2 text-[#006964] font-semibold'>Subject</label>
                   <input
-                    className='border-2 rounded-lg p-3 flex border-gray-300'
+                    className='border-2 rounded-lg p-3 flex border-gray-300 text-[#006964]'
                     type='text'
                     name='subject'
                   />
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label className='uppercase text-sm py-2'>Message</label>
+                  <label className=' text-sm py-2 text-[#006964] font-semibold'>Message</label>
                   <textarea
-                    className='border-2 rounded-lg p-3 border-gray-300'
-                    rows='10'
+                    className='border-2 rounded-lg p-3 border-gray-300 text-[#006964]'
+                    rows='8'
                     name='message'
                   ></textarea>
                 </div>
-                <button className='w-full p-4 text-gray-100 mt-4'>
+                <button className='w-full p-4 text-gray-100 mt-4 animate-pulse'>
                   Send Message
                 </button>
               </form>
@@ -133,10 +133,10 @@ const Contact = () => {
         <div className='flex justify-center py-12'>
           <Link href='/'>
             <a>
-              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-125 ease-in duration-300 bg-[#f8e5dd] hover:bg-[#22adff]'>
                 <HiOutlineChevronDoubleUp
-                  className='text-[#5651e5]'
-                  size={30}
+                  className='text-[#006964] animate-spin'
+                  size={40}
                 />
               </div>
             </a>
