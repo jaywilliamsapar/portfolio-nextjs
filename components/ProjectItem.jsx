b2cb65ef9636ff73842deb8f80e36cb868aa49fe
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2023-06-20 23:06:16 +0800

    Jun20 Update Resume

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -8,7 +8,7 @@ const ProjectItem = ({title, backgroundImg, tech, projectUrl}) => {
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#3dcd49] to-[#7222b9aa]'>
     <Image className='rounded-xl group-hover:opacity-10 hover:animate-ping' src={backgroundImg} alt='/' /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='text-2xl tracking-wider text-center text-[#b932decc] '>{title}</h3>
+        <h3 className='text-2xl tracking-wider text-center text-[#b932decc]'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center font-bold'>{tech}</p>
         <Link href={projectUrl}>
             <p className='text-center py-3 rounded-lg bg-[#111] font-bold text-lg cursor-pointer hover:bg-[#19752d] text-[#c8621a] hover:text-[#fff]'>More Info</p>
---
+++ components/Sparkbang.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Sparkbang = () => {
+  return (
+    <>
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://spankbang.com/7fwca/embed/"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+      ></iframe>
+    </div>
+    </>
+  )
+}
+
+export default Sparkbang 
