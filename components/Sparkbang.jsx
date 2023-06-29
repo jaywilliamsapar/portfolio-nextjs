4a2edd9032e1f60c42dee356aefe4a63a5b60795
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2023-06-29 09:40:46 +0800

    June 29 Add SparkBang

--- components/Sparkbang.jsx
+++ components/Sparkbang.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Sparkbang = () => {
   return (
     <>
-      
+      <iframe
+        width="560"
+        height="315"
+        src="https://spankbang.com/6xbyi/embed/"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+      ></iframe>
     </>
   )
 }
