9c942d08e50732363e7e13c53a2c4ef1f0b1df4f
jaywilliamsapar <jaywilliamsapar@gmail.com>
2023-12-07 14:34:15 -0800

    Update barathrome

--- components/Sparkbang.jsx
+++ components/Sparkbang.jsx
@@ -379,6 +379,14 @@ const Sparkbang = () => {
           scrolling="no"
           allowFullScreen
         ></iframe>
+        <iframe
+          width="560"
+          height="315"
+          src="https://spankbang.com/7q98988o/embed/"
+          frameBorder="0"
+          scrolling="no"
+          allowFullScreen
+        ></iframe>
       </div>
     </>
   )
