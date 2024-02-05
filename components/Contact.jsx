e1c2895672dffe0b1d07452f8b75ebd7c18fe112
jaywilliamsapar <jaywilliamsapar@gmail.com>
2024-02-04 19:47:56 -0800

    Latest update

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
         <p className='text-xl tracking-widest uppercase text-[#5651e5] animate-bounce'>
-          🤠
+          😉
         </p>
         <h2 className='py-4 grad-text'>Get In Touch</h2>
         <div className='grid lg:grid-cols-5 gap-8'>
@@ -73,7 +73,7 @@ const Contact = () => {
           {/* bg-[#e1f0ec] */}
 
           {/* right */}
-          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-500 rounded-xl lg:p-4 bg-gradient-to-r from-[#c1c1c1] to-[#9bb8ba] animate-pulse'>
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-500 rounded-xl lg:p-4 bg-gradient-to-r from-[#c1c1c1] to-[#a2ced1] animate-pulse'>
             <div className='p-4'>
               <form
                 action='https://getform.io/f/9dd3d013-7743-486b-8714-657e8aad7d98' 
