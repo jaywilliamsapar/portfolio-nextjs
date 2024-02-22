b8c092c80f323392b539cac9cf37c09bc6b2d029
jaywilliamsapar <jaywilliamsapar@gmail.com>
2024-02-22 11:06:59 -0800

    Brando laguna bralde

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -8,7 +8,7 @@ const crypto = () => {
   return (
     <div className='w-full'>
       <div className='w-screen h-[50vh] relative'>
-        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/70 z-10' />
+        <div className='absolute top-0 left-0 w-full h-[50vh] bg-black/50 z-10' />
         <Image
           className='absolute z-1'
           layout='fill'
