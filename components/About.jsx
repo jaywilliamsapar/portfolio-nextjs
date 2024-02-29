0db3af45c3ecf7f0abfe12b8d188f59097f7c82c
jaywilliamsapar <jaywilliamsapar@gmail.com>
2024-02-28 21:18:23 -0800

    latest update on css reference

--- components/About.jsx
+++ components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
             that connect with API&#39;s and other backend technologies. I&#39;m
             passionate about learning new technologies and understand there is
             more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using <span className='font-bold text-[#006964] animate-d'>HTML, CSS, Javascript</span>, and <span className='font-bold text-[#006964] animate-d'>React</span>, I am a quick learner and can pick up new tech stacks as
+            in building front-end applications using <span className='font-bold text-[#006964] animate-d'>HTML, CSS, Javascript</span>, and <span className='font-bold text-[#006964] '>React</span>, I am a quick learner and can pick up new tech stacks as
             needed. I believe that being a great developer is not using one
             specific language, but choosing the best tool for the job.
           </p>
