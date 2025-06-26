12c0be8515cf35223315e890f891e03000ee7751
Jaywilliamsapar <jaywilliamsapar@gmail.com>
2025-06-26 22:14:33 +0800

    lakad matatag normalin3x

--- pages/crypto.jsx
+++ pages/crypto.jsx
@@ -18,7 +18,7 @@ const crypto = () => {
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
           <h2 className='py-2'>Medieval-Warfare</h2>
-          <h3>React JS / Tailwind / Firebase</h3>
+          <h3>React JS / Tailwind / Firebase.net</h3>
         </div>
       </div>
 
